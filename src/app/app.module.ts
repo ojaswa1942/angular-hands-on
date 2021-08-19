2545b631d7a93b430a9bd2a72c36aadb708d10cc
Ojaswa Sharma <ojaswa1942@gmail.com>
2021-08-19 14:25:09 +0530

    refactor: delegate details to HeroDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
