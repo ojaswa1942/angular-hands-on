aad868d73b49d00799d1b80893a17e6e991b2996
Ojaswa Sharma <ojaswa1942@gmail.com>
2021-08-19 19:01:20 +0530

    feat: add messaging component and service for event logs

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
+import { MessageService } from 'src/app/services/message.service';
 import { Hero } from 'src/app/services/models/serviceResponse.model';
 
 @Component({
@@ -11,19 +12,23 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroesService: HeroesService) { 
+  constructor(private heroesService: HeroesService, private messageService: MessageService) { 
   }
 
   ngOnInit(): void {
-    this.heroes = this.heroesService.getHeroData();
+    this.heroesService.getHeroData()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect = (hero: Hero): void => {
     if(this.selectedHero && this.selectedHero.id === hero.id) {
-      // unselect
       this.selectedHero = undefined;
+      this.messageService.add(`HeroesComponent: Unselect Hero #${hero.id}`);
     }
-    else this.selectedHero = hero;
+    else {
+      this.selectedHero = hero;
+      this.messageService.add(`HeroesComponent: Select Hero #${hero.id}`);
+    }
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './models/serviceResponse.model';
 import mockHeroData from '../../assets/mock-heroes.json';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,13 @@ import mockHeroData from '../../assets/mock-heroes.json';
 export class HeroesService {
   private allHeroes: Hero[] = [];
   
-  constructor() { 
+  constructor(private messageService: MessageService) { 
     this.allHeroes = mockHeroData;
   }
 
-  getHeroData = (): Hero[] => {
-    return this.allHeroes;
+  getHeroData = (): Observable<Hero[]> => {
+    const heroes = of(this.allHeroes);
+    this.messageService.add("HeroesService: Succesfully loaded heroes!");
+    return heroes;
   }
 }
